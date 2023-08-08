69a1d585b5e5b9e59990123a708e08625de37f18
Peter D <peterd@deakin.edu.au>
2023-08-08 10:14:19 +1000

    fixed 05-sequence-data/0-overview and 1-concepts markdown that broke from migration from docusaurus to astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,19 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	markdown: {
+		shikiConfig: {
+		  // Choose from Shiki's built-in themes (or add your own)
+		  // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+		  theme: 'dracula',
+		  // Add custom languages
+		  // Note: Shiki has countless langs built-in, including .astro!
+		  // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+		  langs: [],
+		  // Enable word wrap to prevent horizontal scrolling
+		  wrap: true,
+		},
+	  },
 	integrations: [
 		starlight({
 			title: 'The Programmer\'s Field Guide',
