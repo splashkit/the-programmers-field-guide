1e76d5fe19c3ea96f2030338269f47113406fbfa
jakerenzella <j@kerenzella.com>
2023-09-10 14:40:28 +1000

    feat: add dynamic image swiper

--- src/components/react/myswiper.jsx
+++ src/components/react/myswiper.jsx
@@ -10,19 +10,20 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 
-export default () => {
+export default ({ images }) => {
+  const imageSlots = [];
+
+  for (const image of images) {
+    imageSlots.push(
+      <SwiperSlide>
+        <img src={image}></img>
+      </SwiperSlide>
+    );
+  }
   return (
     <>
       <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-        <SwiperSlide><img src="/public/test.jpeg"></img></SwiperSlide>
-        <SwiperSlide><img src="/public/test.jpeg"></img></SwiperSlide>
-        <SwiperSlide><img src="/public/test.jpeg"></img></SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        {imageSlots}
       </Swiper>
     </>
   );
