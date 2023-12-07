5104a7e78168c9f1f6fb01b1b761a49289277a9b
Andrew Cain <macite@gmail.com>
2023-12-07 06:44:49 +0000

    fix github link

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ export default defineConfig({
         "./src/styles/custom.css",
       ],
       social: {
-        github: "https://github.com/withastro/starlight",
+        github: "https://github.com/splashkit/the-programmers-field-guide",
       },
       sidebar: [
         {
