b0c4caa4b274fca1236e1dc353c32555e43737a0
omckeon <ordmckeon@gmail.com>
2026-02-28 01:58:51 +1100

    Update MSYS2 Installation script to add vscode settings and remove parts that are duplicated in the splashkit install

--- public/resources/MSYS2_InstallScript.sh
+++ public/resources/MSYS2_InstallScript.sh
@@ -17,50 +17,16 @@ settings_json_url="https://programmers.guide/resources/msys2-settings.json"
 echo "Updating package databases..."
 pacman -Syy
 
-echo "Installing required packages..."
-pacman -S git mingw-w64-x86_64-clang mingw-w64-x86_64-gcc mingw-w64-x86_64-gdb --noconfirm --disable-download-timeout
-
-# -----------------------------------------------------
-# 2. Install .NET SDK
-# -----------------------------------------------------
-
-# Check if .NET SDK is installed
-if ! command -v dotnet &>/dev/null; then
-    echo "Downloading .NET SDK installer..."
-    curl -L $dotnet_sdk_url -o dotnet-sdk-installer.exe
-
-    echo "Installing .NET SDK..."
-    echo "Click "Yes" in pop-up Window if it appears."
-    powershell.exe -Command "Start-Process dotnet-sdk-installer.exe -ArgumentList '/quiet /norestart' -Wait"
-    if [ $? -ne 0 ]; then
-        echo ".NET SDK installation failed. Please try installing it manually. Then run this script again."
-        exit 1
-    fi
-    rm dotnet-sdk-installer.exe
-
-    # Add .NET environment variables
-    if command -v bash &>/dev/null; then
-        if ! grep -q 'export DOTNET_ROOT=$HOME/.dotnet' ~/.bashrc; then
-            echo 'export DOTNET_ROOT=$HOME/.dotnet' >>~/.bashrc
-        fi
-        if ! grep -q 'export PATH=$PATH:$HOME/.dotnet' ~/.bashrc; then
-            echo 'export PATH=$PATH:$HOME/.dotnet' >>~/.bashrc
-        fi
-    fi
-else
-    echo ".NET SDK is already installed."
+if ! command -v git &>/dev/null; then
+    echo "Installing required packages..."
+    pacman -S git --noconfirm --disable-download-timeout
 fi
 
 # -----------------------------------------------------
-# 3. Install SplashKit
+# 2. Install Visual Studio Code
 # -----------------------------------------------------
 
-echo "Installing SplashKit..."
-bash <(curl -s $splashkit_url)
-
-# -----------------------------------------------------
-# 4. Install Visual Studio Code
-# -----------------------------------------------------
+echo ""
 
 # Check if VS Code needs to be installed
 if ! command -v code &>/dev/null; then
@@ -75,7 +41,7 @@ if ! command -v code &>/dev/null; then
     fi
     rm vscode-installer.exe
 else
-    echo "Visual Studio Code is already installed."
+    echo "Visual Studio Code is already installed. Skipping..."
 fi
 
 # Fix issue where .bash_history is not updated with commands used in the integrated VS Code terminal
@@ -83,70 +49,66 @@ if ! grep -q "PROMPT_COMMAND='history -a'" ~/.bashrc; then
     echo "PROMPT_COMMAND='history -a'" >> ~/.bashrc
 fi
 
-# Define paths
-SETTINGS_JSON_PATH="$APPDATA/Code/User"
-VSCODE_PATH=$(
-    cd $LOCALAPPDATA/Programs/Microsoft\ VS\ Code/bin
-    pwd
-) #VS Code path to 'code' (to avoid need for restarting shell)
+# # -----------------------------------------------------
+# # 2. Install .NET SDK (not in use)
+# # -----------------------------------------------------
+
+# # Check if .NET SDK is installed
+# if ! command -v dotnet &>/dev/null; then
+#     echo "Downloading .NET SDK installer..."
+#     curl -L $dotnet_sdk_url -o dotnet-sdk-installer.exe
+
+#     echo "Installing .NET SDK..."
+#     echo "Click "Yes" in pop-up Window if it appears."
+#     powershell.exe -Command "Start-Process dotnet-sdk-installer.exe -ArgumentList '/quiet /norestart' -Wait"
+#     if [ $? -ne 0 ]; then
+#         echo ".NET SDK installation failed. Please try installing it manually. Then run this script again."
+#         exit 1
+#     fi
+#     rm dotnet-sdk-installer.exe
+
+#     # Add .NET environment variables
+#     if command -v bash &>/dev/null; then
+#         if ! grep -q 'export DOTNET_ROOT=$HOME/.dotnet' ~/.bashrc; then
+#             echo 'export DOTNET_ROOT=$HOME/.dotnet' >>~/.bashrc
+#         fi
+#         if ! grep -q 'export PATH=$PATH:$HOME/.dotnet' ~/.bashrc; then
+#             echo 'export PATH=$PATH:$HOME/.dotnet' >>~/.bashrc
+#         fi
+#     fi
+# else
+#     echo ".NET SDK is already installed."
+# fi
 
 # -----------------------------------------------------
-# 5. Replace settings.json file if empty
+# 3. Install SplashKit
 # -----------------------------------------------------
 
 echo ""
-echo "Checking VS Code settings.json file size..."
-cd $SETTINGS_JSON_PATH
 
-# empty settings = <= 5 bytes
-if [ $(wc -c <"settings.json") -le 5 ]; then
-    echo "Adding MSYS2/MINGW64 terminal settings to settings.json file..."
-
-    curl -O "$settings_json_url"
-    mv msys2-settings.json settings.json
+if ! command -v skm &>/dev/null; then
+    echo "Installing SplashKit..."
+    bash <(curl -s $splashkit_url)
 else
-    echo "Cannot add MSYS2/MINGW64 terminal settings to VS Code settings.json file."
-    echo "You will need to update settings.json file manually."
+    echo "SplashKit already installed. Skipping..."
 fi
 
-cd ~
-
 # -----------------------------------------------------
+# 5. Update settings.json file if empty
 # 6. Install VS Code extensions for C++ and C#
 # -----------------------------------------------------
 
-# Install VS Code extensions
-if command -v "$VSCODE_PATH/code" &>/dev/null; then
-    # Check if C/C++ extension pack is installed
-    if ! "$VSCODE_PATH/code" --list-extensions | grep -q "ms-vscode.cpptools-extension-pack"; then
-        echo ""
-        echo "Installing \"C/C++ Extension Pack\" VS Code extension..."
-        "$VSCODE_PATH/code" --install-extension ms-vscode.cpptools-extension-pack
-    fi
+echo ""
 
-    # Check if "C#" extension is installed
-    if ! "$VSCODE_PATH/code" --list-extensions | grep -q "ms-dotnettools.csharp"; then
-        echo ""
-        echo "Installing \"C#\" VS Code extension..."
-        "$VSCODE_PATH/code" --install-extension ms-dotnettools.csharp
-    fi
-
-    # Check if "C# Dev Kit" extension is installed
-    if ! "$VSCODE_PATH/code" --list-extensions | grep -q "ms-dotnettools.csdevkit"; then
-        echo ""
-        echo "Installing \"C# Dev Kit\" VS Code extension..."
-        "$VSCODE_PATH/code" --install-extension ms-dotnettools.csdevkit
-    fi
-
-    # Check if "Python" extension is installed
-    if ! "$VSCODE_PATH/code" --list-extensions | grep -q "ms-python.python"; then
-        echo ""
-        echo "Installing \"Python\" VS Code extension..."
-        "$VSCODE_PATH/code" --install-extension ms-python.python
-    fi
+if ! command -v skm &>/dev/null; then
+    "~/.splashkit/vscode/settings/skm_vscode_settings.sh"
+    "~/.splashkit/vscode/extensions/skm_vscode_extensions.sh"
+else
+    skm vscode settings
+    skm vscode extensions
 fi
 
 # -----------------------------------------------------
 
 echo ""
-echo "Installation complete. Please restart your terminal for changes to take effect."
+echo "Installation complete. You may need to restart your terminal if the \"skm\" command is not found."
